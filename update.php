739f72aef5491d5d1e85b44685c948846976159a
Tim Strijdhorst <tim@decorrespondent.nl>
2020-01-03 23:10:36 +0100

    +make the local apt repo a authenticated repo so we can use apt install in the default secure way

--- update.php
+++ update.php
@@ -3,8 +3,9 @@
 (new ProtonMailDebianRepoBuilder())->update();
 
 class ProtonMailDebianRepoBuilder {
-	private const PKGBUILD_URL = 'https://protonmail.com/download/beta/PKGBUILD';
-	private const REPO_DIR     = __DIR__.'/repo';
+	private const PKGBUILD_URL  = 'https://protonmail.com/download/beta/PKGBUILD';
+	private const REPO_DIR      = __DIR__.'/repo';
+	private const GPG_SIGN_USER = 'ProtonMailRepoSignKey';
 	
 	public function update() {
 		preg_match('/source=\("(.*)"\)/', file_get_contents(self::PKGBUILD_URL), $matches);
@@ -27,7 +28,17 @@ class ProtonMailDebianRepoBuilder {
 		echo 'Downloading: '.$latestDebFileURL.PHP_EOL;
 		
 		file_put_contents($latestDebFilePath, fopen($latestDebFileURL, 'r'));
-	
+		
 		echo 'Download finished'.PHP_EOL;
+		
+		echo 'Updating local apt repo'.PHP_EOL;
+		chdir(self::REPO_DIR);
+		exec('dpkg-scanpackages . /dev/null > Packages');
+		exec('gzip --keep --force -9 Packages');
+		exec('apt-ftparchive release . > Release');
+		exec('gpg --clearsign -o InRelease --local-user '.escapeshellarg(self::GPG_SIGN_USER).' Release');
+		exec('gpg -abs -o Release.gpg --local-user '.escapeshellarg(self::GPG_SIGN_USER).' Release');
+		
+		echo 'Local apt repo updated. Run apt-get update && apt-get upgrade to install new version'.PHP_EOL;
 	}
 }
